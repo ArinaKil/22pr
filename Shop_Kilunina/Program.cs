4e917e6609c711418651606a899745746dd76784
ArinaKil <arikakilka@mail.ru>
2026-06-12 15:58:56 +0500

    Разработанный контроллер предметов. Килунина

--- Shop_Kilunina/Program.cs
+++ Shop_Kilunina/Program.cs
@@ -1,4 +1,9 @@
+using Shop_Kilunina.Data.Interfaces;
+using Shop_Kilunina.Data.Mocks;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<ICategorys, MockCaregorys>();
+builder.Services.AddTransient<IItems, MockItems>();
 builder.Services.AddMvc(option => option.EnableEndpointRouting = false);
 var app = builder.Build();
 
